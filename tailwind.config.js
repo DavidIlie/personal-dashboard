cf6335bf0037cf2d68642153ebb036069aceca4e
David Ilie <david@davidilie.com>
2021-07-21 17:58:27 +0200

    fix responsiveness on xps 13

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     purge: ["./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                "3xl": "1792px",
+            },
+        },
     },
     variants: {
         extend: {},
