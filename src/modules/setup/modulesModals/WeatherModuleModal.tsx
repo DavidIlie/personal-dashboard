65c5caab888081400beacceea50a557a3ad86ed6
David Ilie <david@davidilie.com>
2021-10-07 09:45:44 +0200

    (chores): got modal design

--- src/modules/setup/modulesModals/WeatherModuleModal.tsx
+++ src/modules/setup/modulesModals/WeatherModuleModal.tsx
@@ -1,3 +1,8 @@
+import { Form, Field, Formik } from "formik";
+
+import Label from "@ui/form/Label";
+import Input from "@ui/form/Input";
+
 import Modal from "@ui/Modal";
 
 interface Props {
@@ -13,13 +18,65 @@ const WeatherModuleModal = ({
         <Modal
             isOpen={isOpen}
             updateModalState={updateModalState}
-            title="Insert API Key"
+            title="Insert Weather API key"
         >
             <>
                 <h1 className="mb-1">
-                    It is required that you put an API key in order to enable
-                    this module.
+                    You need to enter an API key from{" "}
+                    <a
+                        href="https://openweathermap.org"
+                        target="_blank"
+                        className="text-blue-400 hover:text-blue-500 duration-200"
+                    >
+                        openweathermap.org
+                    </a>{" "}
+                    in order to use this module. If you don't understand how to
+                    do it, check out the{" "}
+                    <a
+                        href="https://github.com/davidilie/personal-dashboard"
+                        target="_blank"
+                        className="text-blue-400 hover:text-blue-500 duration-200"
+                    >
+                        wiki
+                    </a>
+                    .
                 </h1>
+
+                <Formik
+                    validateOnChange={false}
+                    validateOnBlur={false}
+                    initialValues={{ api_key: "" }}
+                    onSubmit={async (data, { setSubmitting, resetForm }) => {}}
+                >
+                    {({ errors, isSubmitting }) => (
+                        <Form>
+                            <div className="mt-2">
+                                <Label>API Key</Label>
+                                <Field
+                                    name="api_key"
+                                    required
+                                    darker
+                                    as={Input}
+                                />
+                            </div>
+                            <div className="mt-4 flex justify-end gap-2">
+                                <button
+                                    type="button"
+                                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-100 bg-blue-100 bg-dark-gray-900 border border-transparent rounded-md hover:bg-dark-gray-800 duration-150 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                                    onClick={updateModalState}
+                                >
+                                    Nevermind
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-100 bg-gray-900 border border-transparent rounded-md hover:bg-dark-gray-900 duration-150 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                                >
+                                    {isSubmitting ? "Updating" : "Update"}
+                                </button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </>
         </Modal>
     );
--- src/ui/form/Input.tsx
+++ src/ui/form/Input.tsx
@@ -1,12 +1,20 @@
 interface InputProps {
     input?: string;
     required?: boolean;
+    darker?: boolean;
 }
 
-const Input = ({ input, required, ...rest }: InputProps): JSX.Element => {
+const Input = ({
+    input,
+    required,
+    darker,
+    ...rest
+}: InputProps): JSX.Element => {
     return (
         <input
-            className="w-full py-2 px-3 text-base border rounded-lg placeholder-gray-600 bg-gray-800 border-gray-700 text-gray-400 focus:outline-none focus:border-gray-700 focus:ring-opacity-50"
+            className={`w-full py-2 px-3 text-base border rounded-lg placeholder-gray-600 bg-gray-${
+                darker ? "900" : "800"
+            } border-gray-700 text-gray-400 focus:outline-none focus:border-gray-700 focus:ring-opacity-50`}
             type={input}
             required={required}
             {...rest}
